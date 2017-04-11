c22b7aa80d5fbfcf2a0631a4f91872be6a3a56d0
Jerahmeel Anibor <jerahmeelanibor@gmail.com>
2017-04-11 09:56:34 +0100

    Add files via upload
    The Anibor Library is open for improvements.

---
+++ anibor.php
@@ -0,0 +1,79 @@
+<?php
+
+class anibor{
+    var $error_in_connection = 'Unable to connect to database at this time.';
+    
+    function db_connect(){
+        try{
+            // Establishes a database connection
+            $dsn = 'mysql:host=localhost; dbname=anibor; charset=latin1';
+            $username = 'root';
+            $password = '';
+            $connection = new PDO($dsn, $username, $password);
+
+            return $connection;
+        }
+        catch(Exception $ex){ echo $error_in_connection; }
+    } 
+    //
+    // CRUD functions...
+    // Insert and update
+    function tranzact($statement, $value = array()){
+        // ...passes the sql statements and insert the value into the array
+        // ... the function performs CRUD functions
+        try {
+            $db = $this->db_connect();
+            $insert_data = $db->prepare($statement);
+            for ($i = 1; $i <= count($value); $i++){
+                $insert_data->bindValue($i, $value[$i-1]);
+            }
+            $insert_data->execute(); // Outside the loop to bind all the values so as to avoid database inconsistency
+        }
+        catch (Exception $ex) {
+            echo $this->error_in_connection;
+        }
+    }
+
+    // Retrieving a value   
+    function retrieve($statement, $column){
+        try{
+            $db = $this->db_connect();
+
+            foreach ($db->query($statement) as $row) return $row[$column]; 
+        }
+        catch(Exception $ex){}
+    }
+
+    // Delete
+    function delete($statement){
+        try{
+            $db = $this->db_connect();
+            return $db->query($statement);
+        }
+        catch(Exception $ex){}
+    }               
+    
+    // Summing numerical value of columns
+    function sum($statement, $column){
+        try{
+            $sum = 0;
+            $db = $this->db_connect();
+            foreach ($select = $db->query($statement) as $row) $sum+= $row[$column]; 
+            return $sum; 
+        }
+        catch (Exception $ex){}
+    } 
+    
+    // Getting rows
+    function get_rows($statement){
+        try{
+            $db = $this->db_connect();
+            $query = $db->query($statement);
+            $row_count = $query->rowCount();
+
+           return $row_count;
+        }
+        catch(Exception $ex){}
+    }    
+    // End CRUD functions
+}
